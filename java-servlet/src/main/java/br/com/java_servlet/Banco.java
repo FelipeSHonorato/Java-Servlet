bb84b8d6f49f7c90281fba336d5f0421672d4350
FelipeSHonorato <felipeshonorato@gmail.com>
2021-09-18 15:17:06 -0300

    Criando lista de empresas cadastradas

--- java-servlet/src/main/java/br/com/java_servlet/Banco.java
+++ java-servlet/src/main/java/br/com/java_servlet/Banco.java
@@ -6,6 +6,20 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	
+	/** Criando um static para gerar no início do programa duas empresas **/
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("Alura");
+		
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("BabyMobel");
+		
+		lista.add(empresa);
+		lista.add(empresa2);
+	}
+	
+	
 
 	public void adiciona(Empresa empresa) {
 		lista.add(empresa);
